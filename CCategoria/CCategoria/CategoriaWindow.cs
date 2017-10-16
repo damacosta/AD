2026e2d9204e8ea8dda8adf14978be5ae295accc
damacosta <2dam.acosta@institutoserpis.org>
2017-10-16 12:09:14 +0200

    Subido CCategoria con boton de refresh y refactorizado

---
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace CCategoria{
+    
+    public partial class CategoriaWindow : Gtk.Window
+    {
+        public CategoriaWindow() :
+                base(Gtk.WindowType.Toplevel)
+        {
+			this.Build();
+
+			saveAction.Activated += delegate {
+				string nombre = entryNombre.Text;
+				IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+				dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+                DbCommandHelper.AddParameter(dbCommand, "nombre", nombre);
+		
+				dbCommand.ExecuteNonQuery();
+				Destroy();
+			};
+		}
+	}
+}
---
+++ CCategoria/CCategoria/MainWindow.cs
@@ -0,0 +1,52 @@
+﻿using Gtk;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using CCategoria;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        App.Instance.Connection = new MySqlConnection("server=localhost;database=dbprueba;user=root;password=sistemas");
+        App.Instance.Connection.Open();
+
+        treeView.AppendColumn("id", new CellRendererText(), "text", 0);
+        treeView.AppendColumn("nombre", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+
+        treeView.Model = listStore;
+
+        fillListStore(listStore);
+
+        newAction.Activated += delegate {
+			new CategoriaWindow();
+		};
+
+        refreshAction.Activated += delegate {
+            listStore.Clear();
+            fillListStore(listStore);
+        };
+
+	}
+
+    private void fillListStore(ListStore listStore){
+        IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+        dbCommand.CommandText = "select * from categoria order by id";
+        IDataReader dataReader = dbCommand.ExecuteReader();
+        while (dataReader.Read())
+            listStore.AppendValues(dataReader["id"], dataReader["nombre"]);
+        dataReader.Close();
+
+    }
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		App.Instance.Connection.Close();
+
+		Application.Quit();
+		a.RetVal = true;
+	}
+}
