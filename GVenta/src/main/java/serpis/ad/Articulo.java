3208b5db1b68b6c0dd7fe077036a269966158eb9
damacosta <2dam.acosta@institutoserpis.org>
2018-02-12 12:13:44 +0100

    Actualizado GVenta

--- GVenta/src/main/java/serpis/ad/Articulo.java
+++ GVenta/src/main/java/serpis/ad/Articulo.java
@@ -1,54 +1,119 @@
 package serpis.ad;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-import javax.persistence.*;
 
 @Entity
-public class Articulo {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
-	private String nombre;
-	private BigDecimal precio;
-	@ManyToOne
-	@JoinColumn(name="categoria")
-	private Categoria categoria;
+@Table(name="articulo"
+    ,catalog="dbprueba"
+    , uniqueConstraints = @UniqueConstraint(columnNames="nombre") 
+)
+public class Articulo  implements java.io.Serializable {
+
+
+     private Long id;
+     private Categoria categoria;
+     private String nombre;
+     private BigDecimal precio;
+     private Set<Pedidolinea> pedidolineas = new HashSet<Pedidolinea>(0);
+
+    public Articulo() {
+    }
 
 	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	public Categoria getCategoria() {
-		return categoria;
-	}
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public BigDecimal getPrecio() {
-		return precio;
-	}
-	public void setPrecio(BigDecimal precio) {
-		this.precio = precio;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("[%s] %s %s€ (%s)", id, nombre, precio, categoria);
+    public Articulo(String nombre, BigDecimal precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    public Articulo(Categoria categoria, String nombre, BigDecimal precio, Set<Pedidolinea> pedidolineas) {
+       this.categoria = categoria;
+       this.nombre = nombre;
+       this.precio = precio;
+       this.pedidolineas = pedidolineas;
+    }
+   
+     @Id @GeneratedValue(strategy=IDENTITY)
 
-	}
+    
+    @Column(name="id", unique=true, nullable=false)
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="categoria")
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+    
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    
+    @Column(name="nombre", unique=true, nullable=false, length=50)
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    @Column(name="precio", nullable=false, precision=10)
+    public BigDecimal getPrecio() {
+        return this.precio;
+    }
+    
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="articulo")
+    public Set<Pedidolinea> getPedidolineas() {
+        return this.pedidolineas;
+    }
+    
+    
+    public void setPedidolineas(Set<Pedidolinea> pedidolineas) {
+        this.pedidolineas = pedidolineas;
+    }
+    
+    public String toString() {
+    	if(this.getCategoria()!= null) {
+		return String.format("%-5s%-15s%-15s%-15s", String.valueOf(this.getId()),
+							this.getCategoria().getNombre(),
+							this.getNombre(),
+							String.valueOf(this.getPrecio()));
+		
+    	}else {
+    		return String.format("%-5s%-15s%-15s%-15s", String.valueOf(this.getId()),
+    				"",
+    				this.getNombre(),
+					String.valueOf(this.getPrecio()));
+    		
+    	}
+    
+    	
+    }
+   
+
 }
--- GVenta/src/main/java/serpis/ad/Categoria.java
+++ GVenta/src/main/java/serpis/ad/Categoria.java
@@ -1,31 +1,82 @@
 package serpis.ad;
 
-import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
-public class Categoria {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
-	private String nombre;
+@Table(name="categoria"
+    ,catalog="dbprueba"
+    , uniqueConstraints = @UniqueConstraint(columnNames="nombre") 
+)
+public class Categoria  implements java.io.Serializable {
+
+
+     private Long id;
+     private String nombre;
+     private Set<Articulo> articulos = new HashSet<Articulo>(0);
+
+    public Categoria() {
+    }
+
 	
-	public long getId() {
-		return id;}
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+    public Categoria(String nombre, Set<Articulo> articulos) {
+       this.nombre = nombre;
+       this.articulos = articulos;
+    }
+   
+     @Id @GeneratedValue(strategy=IDENTITY)
 
-	public void setId(long id) {
-		this.id = id;}
+    
+    @Column(name="id", unique=true, nullable=false)
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;}
+    
+    @Column(name="nombre", unique=true, nullable=false, length=50)
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;}
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="categoria")
+    public Set<Articulo> getArticulos() {
+        return this.articulos;
+    }
+    
+    public void setArticulos(Set<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+    
+    @Override
+    public String toString(){
+    	return String.format("%-5s%-15s", String.valueOf(this.getId()),
+				this.getNombre());
+				
+    	
+    }
 
-@Override
-public String toString() {
-	return String.format("[%s] %s", id, nombre);
 
-}
 
 
 }
